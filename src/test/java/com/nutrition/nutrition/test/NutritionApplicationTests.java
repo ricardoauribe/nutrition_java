4efc9fb781314aff6f10ff86724e5ac71eaff8e9
Ricardo <ricardoauribe@hotmail.com>
2021-07-12 20:44:31 -0500

    Test for context loads

--- src/test/java/com/nutrition/nutrition/test/NutritionApplicationTests.java
+++ src/test/java/com/nutrition/nutrition/test/NutritionApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nutrition.nutrition;
+package com.nutrition.nutrition.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
---
+++ src/test/java/com/nutrition/nutrition/test/NutritionTests.java
@@ -0,0 +1,12 @@
+package com.nutrition.nutrition.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class NutritionTests {
+  @Test
+  public void contextLoads(){
+
+  }
+}
