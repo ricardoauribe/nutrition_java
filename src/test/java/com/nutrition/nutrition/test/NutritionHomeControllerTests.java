1489652cdcacfa0720751fcfcdcfcdac2751903d
Ricardo <ricardoauribe@hotmail.com>
2021-07-13 21:51:06 -0500

    Added test case for patient endpoint

--- src/test/java/com/nutrition/nutrition/test/NutritionHomeControllerTests.java
+++ src/test/java/com/nutrition/nutrition/test/NutritionHomeControllerTests.java
@@ -6,13 +6,19 @@ import java.net.URL;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class NutritionHomeControllerTests extends NutritionApplicationTests {
 
   @LocalServerPort
@@ -21,8 +27,11 @@ public class NutritionHomeControllerTests extends NutritionApplicationTests {
   @Autowired
   private TestRestTemplate restTemplate;
 
+  @Autowired
+  private MockMvc mockMvc;
+
   @Test
-  public void testPatient() throws Exception {
+  public void testHome() throws Exception {
 
     ResponseEntity<String> response = restTemplate.getForEntity(
         new URL("http://localhost:" + port + "/").toString(), String.class);
@@ -30,16 +39,15 @@ public class NutritionHomeControllerTests extends NutritionApplicationTests {
 
   }
 
-//TODO: use json assert to review test case content
-/*
+
   @Test
-  public void testPatient2() throws Exception {
+  public void testPatient() throws Exception {
     mockMvc.perform(get("/patient")).andExpect(status().isOk())
-      .andExpect(content().contentType("application/json;charset=UTF-8"))
+      .andExpect(content().contentType("application/json"))
       .andExpect(jsonPath("$.name").value("Ricardo"))
       .andExpect(jsonPath("$.weight").value("70"))
       .andExpect(jsonPath("$.age").value("myAge"));
 
   }
-  */
+  
 }
